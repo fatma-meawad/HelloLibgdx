de3a49e093c4fed17a26aab59cbf9da7ae813b15
Ahmed Fahmy <a7med.fahmy94@gmail.com>
2015-06-11 19:42:41 +0200

    added & loaded assets

--- core/src/com/fahmy/drop/Drop.java
+++ core/src/com/fahmy/drop/Drop.java
@@ -2,26 +2,35 @@ package com.fahmy.drop;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+
 public class Drop extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+	private Texture dropImage;
+	private Texture bucketImage;
+	private Sound dropSound;
+	private Music backgroundMusic;
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		//load the images of the droplet and the bucket (64x64)
+		dropImage = new Texture(Gdx.files.internal("droplet.png"));
+		bucketImage = new Texture(Gdx.files.internal("bucket.png"));
+
+		//load the sound effect of the drop
+		dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.mp3"));
+
+		//load the background music and play it
+		backgroundMusic = Gdx.audio.newMusic(Gdx.files.internal("Barbarian.mp3"));
+		backgroundMusic.setLooping(true);
+		backgroundMusic.play();
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
 	}
 }
